13abbdc495b73f5721ca54da79ad366954cde56c
saurabhjain9070 <tiwefe7341@0pppp.com>
2022-10-24 17:55:16 +0530

    Fix API Issue

--- src/redux/todo/TodoTypes.js
+++ src/redux/todo/TodoTypes.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3001';
+export const BASE_URL = 'https://wary-spangled-howler.glitch.me/';
 export const ADD_TODOS = 'ADD_TODOS';
 export const GET_TODOS = 'GET_TODOS';
 export const DELETE_TODOS = 'DELETE_TODOS';
